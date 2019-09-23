319697342105e1a72fd96a3854215953bf9be433
oldblackeye <kasperg3@rocketmail.com>
2019-09-23 14:40:54 +0200

    Working cmake

--- include/URVrepSim.h
+++ include/URVrepSim.h
@@ -16,15 +16,26 @@
 #include <geometry_msgs/Transform.h>
 #include <std_msgs/Float64.h>
 
+//Collision detection
+#include <rwlibs/proximitystrategies/ProximityStrategyFactory.hpp>
+
 class URVrepSim {
     using Q = rw::math::Q;
 
 public:
     URVrepSim();
-
-
+    Q getQ();
+    bool setQ(Q);
+    bool moveQ(Q);
+    bool moveHome();
+    rw::kinematics::State getState();
+    rw::models::Device::Ptr getDevice();
 private:
-
+    ros::NodeHandle nh;
+    rw::models::WorkCell::Ptr wc;
+    rw::models::Device::Ptr device;
+    rw::kinematics::State state;
+    rw::proximity::CollisionDetector::Ptr detector;
 
 };
 
--- src/URVrepSim.cpp
+++ src/URVrepSim.cpp
@@ -6,19 +6,41 @@
 
 
 URVrepSim::URVrepSim() {
-
-
-
+    //auto packagePath = ros::package::getPath("");
+    //wc = rw::loaders::WorkCellLoader::Factory::load(packagePath + "/WorkCell/Scene.wc.xml");
+    //device = wc->findDevice("UR5");
+    //state = wc->getDefaultState();
+    //detector = new rw::proximity::CollisionDetector(wc, rwlibs::proximitystrategies::ProximityStrategyFactory::makeDefaultCollisionStrategy());
 }
 
+URVrepSim::Q URVrepSim::getQ() {
+    return URVrepSim::Q();
+}
 
+bool URVrepSim::setQ(URVrepSim::Q) {
+    return true;
+}
 
+bool URVrepSim::moveQ(URVrepSim::Q) {
+    return true;
+}
+
+bool URVrepSim::moveHome() {
+    return true;
+}
+
+rw::kinematics::State URVrepSim::getState() {
+    return state;
+}
+
+rw::models::Device::Ptr URVrepSim::getDevice() {
+    return device;
+}
+
+//Execute what the node should do underneath
 int main(){
     std::cout << "Hello World!" << std::endl;
 
 
+
 }
-
-
-
-
