17e7021e969a3ed197fdb5dee440af9c897ee62f
kasperg3 <kasperg3@rocketmail.com>
2020-02-28 15:40:58 +0100

    Reset to DDPG-HER

--- single_ur.py
+++ single_ur.py
@@ -13,6 +13,12 @@ from replay import Memory
 from agent import Noise
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+tf.get_logger().warning('test')
+# WARNING:tensorflow:test
+tf.get_logger().setLevel('ERROR')
+tf.get_logger().warning('test')
+
 # function to unpack observation from gym environment
 def unpackObs(obs):
     return  obs['achieved_goal'], \
@@ -91,52 +97,57 @@ def main(args):
         epoch_test_history = []
         for k in range(args['epochs']):
             score_history = []
-            for i in range(args['episodes']):
-                achieved_goal, desired_goal, state, state_prime = unpackObs(env.reset())
-                done = False
-                episode_score = 0
-                for j in range(int(args['episode_length'])):
-                    if bool(np.random.binomial(1, 0.7)): # 30% random action
-                        act = agent.choose_action(state=state, env=env, test=True)
-                    else:
-                        act = agent.random_action()
-                    new_obs, reward, done, info = env.step(act[0])
-                    achieved_goal, desired_goal, state_next, state_prime_next = unpackObs(new_obs)
+            for c in range(args['cycles']):
+                for i in range(args['episodes']):
+                    achieved_goal, desired_goal, state, state_prime = unpackObs(env.reset())
+                    done = False
+                    episode_score = 0
+                    for j in range(int(args['episode_length'])):
+                        if bool(np.random.binomial(1, 0.2)):
+                            act = agent.random_action()
+                        else:
+                            act = agent.choose_action(state=state, test=args['test'])
 
-                    # Store data in replay buffer
-                    agent.remember(state, state_next, act[0], reward, done)
-                    if bool(np.random.binomial(1, 0.8)):
-                        agent.rememberHER(state_prime, state_prime_next, achieved_goal, info, act[0], env)
+                        new_obs, reward, done, info = env.step(act[0])
+                        achieved_goal, desired_goal, state_next, state_prime_next = unpackObs(new_obs)
 
-                    agent.learn()
+                        # Store data in replay buffer
+                        agent.remember(state, state_next, act[0], reward, done)
+                        if bool(np.random.binomial(1, 0.8)):
+                            agent.rememberHER(state_prime, state_prime_next, achieved_goal, info, act[0], env)
 
-                    #Update the next state and add reward to episode_score
-                    episode_score += reward
-                    state = state_next
+                        #Update the next state and add reward to episode_score
+                        episode_score += reward
+                        state = state_next
 
-                    # render episode
-                    if args['render']:
-                        env.render()
+                        # render episode
+                        if args['render']:
+                            env.render()
 
-                #Save the episode scores
-                score_history.append(episode_score)
-                #print('epoch:' + str(k) + ' | episode: ', str(i), ' | score %.2f' % episode_score, )
-            #Rollouts
+                    #Save the episode scores
+                    score_history.append(episode_score)
+                    for t in range(int(args['optimizationsteps'])):
+                        agent.learn()
+
+                    #Print the Episode score
+                    print('epoch:' + str(k) + ' | cycle:' + str(c) + ' | episode:' + str(i) +" | episode score: %.2f" % (episode_score))
+
             test_history = []
-            for _l in range(int(args['rollouts'])):
+            for _ in range(int(args['rollouts'])):
                 achieved_goal, desired_goal, state, state_prime = unpackObs(env.reset())
                 done = False
                 episode_score = 0
-                for j in range(int(args['episode_length'])):
-                    act = agent.choose_action(state=state, env=env, test=args['test'])
+                for _ in range(int(args['episode_length'])):
+                    act = agent.choose_action(state=state, test=bool(args['test']))
                     new_obs, reward, done, info = env.step(act[0])
                     achieved_goal, desired_goal, state_next, state_prime_next = unpackObs(new_obs)
                     episode_score += reward
                     state = state_next
                 test_history.append(episode_score)
-            print('epoch:' + str(k) + " | score: %.2f | test score: %.2f " % (np.mean(score_history), np.mean(test_history)) )
 
-            #Save the histories of the epoch, both test and training
+            print('epoch:' + str(k) + " | mean score: %.2f | mean test score: %.2f " %(np.mean(score_history), np.mean(test_history)))
+
+            # Save the histories of the epoch, both test and training
             epoch_test_history.append(test_history)
             epoch_history.append(score_history)
 
@@ -180,21 +191,22 @@ if __name__ == '__main__':
     # training parameters
     parser.add_argument('--actor-lr', help='actor learning rate', default=0.0001)
     parser.add_argument('--critic-lr', help='critic learning rate', default=0.001)
-    parser.add_argument('--batch-size', help='batch size', default=64)
+    parser.add_argument('--batch-size', help='batch size', default=256)
     parser.add_argument('--gamma', help='discount factor reward', default=0.99)
     parser.add_argument('--tau', help='target update tau', default=0.001)
     parser.add_argument('--memory-size', help='size of the replay memory', default=1000000)
-    parser.add_argument('--hidden-sizes', help='number of nodes in hidden layer', default=(400, 300))
-    parser.add_argument('--episode-length', help='max length of 1 episode', default=150)
-    parser.add_argument('--episodes', help='episodes to train', default=20)
-    parser.add_argument('--epochs', help='number of epochs', default=100)
-    parser.add_argument('--rollouts', help='Number of rollouts pr epoch', default=10)
-
+    parser.add_argument('--hidden-sizes', help='number of nodes in hidden layer', default=(256, 256, 256))
+    parser.add_argument('--epochs', help='number of epochs', default=50)
+    parser.add_argument('--cycles', help='number of cycles to run in each epoch', default=50)
+    parser.add_argument('--episodes', help='episodes to train in a cycle', default=16)
+    parser.add_argument('--episode-length', help='max length of 1 episode', default=100)
+    parser.add_argument('--optimizationsteps', help='number of optimization steps', default=40)
+    parser.add_argument('--rollouts', help='Number of rollouts to run each epoch', default=10)
     # others and defaults
-    parser.add_argument('--seed', help='random seed', default=1235)
+    parser.add_argument('--seed', help='random seed', default=1234)
     parser.add_argument('--render', help='render the gym env', action='store_true')
     parser.add_argument('--test', help='test mode does not do exploration', action='store_true')
-    parser.add_argument('--variation', help='model variation name', default='DDPG_HER')
+    parser.add_argument('--variation', help='model variation name', default='DDPG_HER_TEST')
     #parser.set_defaults(env='FetchReach-v1')
     #parser.set_defaults(env='mergablerobots-v0')
     parser.set_defaults(env='UrReach-v0')
@@ -202,7 +214,6 @@ if __name__ == '__main__':
     parser.set_defaults(render=False)
     parser.set_defaults(test=False)
 
-    #test
     # parse arguments
     args = vars(parser.parse_args())
 
