77199d8bd5e3230b2be7517648f5ea65b730eca3
nikolajottosen <nikolaj_ottosen@hotmail.com>
2020-04-09 11:05:37 +0200

    Added plot file, Success counter in test-main, Sat standard to 2m samples

--- plot_file.py
+++ plot_file.py
@@ -2,18 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 #pnp_load = np.load('results/TD3_UrBinPickingPlace-v0_1234_train.npy')
-place_load = np.load('results/TD3_UrBinPickingPlace-v0_1234_test.npy')
-orient_load = np.load('results/TD3_UrBinPickingOrient-v0_1234_test.npy')
-lift_load = np.load('results/TD3_UrBinPickingLift-v0_1234_test.npy')
-reach_load = np.load('results/TD3_UrBinPickingReach-v0_1234_test.npy')
-plot_load = lift_load[:400]
-
+place1_load = np.load('results/rot_and_is_failed/TD3_UrBinPickingOrient-v0_1234_test.npy')
+place2_load = np.load('results/rot_no_is_failed/TD3_UrBinPickingOrient-v0_1234_test.npy')
+#lift_load = np.load('results/TD3_UrBinPickingLift-v0_1234_test.npy')
+#reach_load = np.load('results/TD3_UrBinPickingReach-v0_1234_test.npy')
+plot_load = place1_load[:400]
+plot2_load = place2_load[:400]
 
 positions = [0, 100, 200, 300, 400] #
 labels = [0, 0.5, 1, 1.5, 2]
 plt.xticks(positions, labels)
 plt.plot(plot_load, label='Test score')
+plt.plot(plot2_load, label='Test score')
 plt.ylabel('Reward')
 plt.xlabel('Samples in millions')
-plt.legend(["Lift"])
+plt.legend(["Orient_w_is_failed", "Orient_wo_is_failed"])
 plt.show()
